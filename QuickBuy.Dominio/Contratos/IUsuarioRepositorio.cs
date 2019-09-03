8bdb34b7c58b784aed2bb7478f8b7c80fbde027b
Guilherme Baldo Pedrosa <pedrosa.gui@hotmail.com>
2019-09-02 22:15:28 -0300

    Repositorios

---
+++ QuickBuy.Dominio/Contratos/IUsuarioRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using QuickBuy.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Contratos
+{
+    public interface IUsuarioRepositorio : IBaseRepositorio<Usuario>
+    {
+
+
+    }
+}
